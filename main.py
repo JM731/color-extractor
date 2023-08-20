6a74c618549126f837311f46525cf31df02f8c40
JM731 <137689384+JM731@users.noreply.github.com>
2023-08-20 20:20:37 -0300

    Update main.py
    Remove unnecessary Flask configurations.

--- main.py
+++ main.py
@@ -53,8 +53,6 @@ class UploadForm(FlaskForm):
 
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = "SomeSecretKey"
 
 
